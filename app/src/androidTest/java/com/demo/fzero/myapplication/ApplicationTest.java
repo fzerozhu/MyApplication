8877f78d7e4d480c3195ef2dfae8e895469810bf
fzero <fzerozhu@163.com>
2016-04-28 16:51:25 +0800

    bbbb

--- app/src/androidTest/java/com/demo/fzero/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/demo/fzero/myapplication/ApplicationTest.java
@@ -10,5 +10,6 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
         String a="";
+        String b="";
     }
 }
